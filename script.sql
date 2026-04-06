303e7501ae2c4d51d07aca67beba49db282a3376
seungjin <xeungjin@nate.com>
2026-04-06 15:48:46 +0900

    create class/takes table

--- script.sql
+++ script.sql
@@ -1,3 +1,30 @@
+CREATE TABLE takes (
+	stu_id varchar2(10),
+	class_id varchar2(10),
+	grade char(5),
+	CONSTRAINT pk_takes PRIMARY key(stu_id, class_id),
+	CONSTRAINT fk_takes1 FOREIGN key(stu_id)
+	REFERENCES student(stu_id),
+	CONSTRAINT fk_takes2 FOREIGN key(class_id)
+	REFERENCES class(class_id)
+)
+
+CREATE TABLE class (
+	class_id varchar2(10),
+	course_id varchar2(10),
+	YEAR int,
+	semester int,
+	division char(1),
+	prof_id varchar2(10),
+	classroom varchar2(9),
+	enroll int,
+	CONSTRAINT pk_class PRIMARY key(class_id),
+	CONSTRAINT fk_class1 FOREIGN key(course_id)
+	REFERENCES course(course_id),
+	CONSTRAINT fk_class2 FOREIGN key(prof_id)
+	REFERENCES professor(prof_id)
+)
+
 CREATE TABLE course (
 	course_id varchar2(10),
 	title varchar2(14) NOT NULL,
